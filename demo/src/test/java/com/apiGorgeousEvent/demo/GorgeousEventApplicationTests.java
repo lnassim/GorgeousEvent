5fb2562e07a26177ee496ccbbb9b6c343cf9ca5f
Lara <laranassim24@gmail.com>
2020-12-15 20:29:20 +0100

    Ajout de fichiers manquants structure

---
+++ demo/src/test/java/com/apiGorgeousEvent/demo/GorgeousEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiGorgeousEvent.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorgeousEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
